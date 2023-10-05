cf66be2af8e4ab405e0a94358a8b85a0d14bd094
Gard <gaf1902@outlook.com>
2023-10-05 12:31:25 +0200

    Test endpoint product

--- src/main/java/com/experis/no/boxinator/controllers/productController.java
+++ src/main/java/com/experis/no/boxinator/controllers/productController.java
@@ -1,10 +1,29 @@
 package com.experis.no.boxinator.controllers;
 
+import com.experis.no.boxinator.mappers.ProductMapper;
+import com.experis.no.boxinator.services.product.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/v1/product")
 public class productController {
+    private final ProductService productService;
+    private final ProductMapper productMapper;
+
+    public productController(ProductService productService, ProductMapper productMapper) {
+        this.productService = productService;
+        this.productMapper = productMapper;
+    }
+   @GetMapping
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok(
+                productMapper.productToProductDTO(
+                        productService.findAll()
+                )
+        );
+    }
 
 }
