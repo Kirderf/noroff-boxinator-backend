13682009ad6ee8fadfa658ce20a6650772c4ccc9
Fredrik Holm Julsen <Fredrik.julsen71@gmail.com>
2023-10-05 09:23:00 +0200

    Repo and Service init

---
+++ src/main/java/com/experis/no/boxinator/services/orders/OrdersService.java
@@ -0,0 +1,7 @@
+package com.experis.no.boxinator.services.orders;
+
+import com.experis.no.boxinator.models.Orders;
+import com.experis.no.boxinator.services.CrudService;
+
+public interface OrdersService extends CrudService<Orders,Integer> {
+}
---
+++ src/main/java/com/experis/no/boxinator/services/user/UserService.java
@@ -0,0 +1,7 @@
+package com.experis.no.boxinator.services.user;
+
+import com.experis.no.boxinator.models.User;
+import com.experis.no.boxinator.services.CrudService;
+
+public interface UserService extends CrudService<User,Integer> {
+}
