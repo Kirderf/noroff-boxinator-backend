86f1f520f31f10b69032e6822c72e494a9ed312e
Fredrik Holm Julsen <Fredrik.julsen71@gmail.com>
2023-10-17 13:38:21 +0200

    Endpoint for updateByEmail

---
+++ src/main/java/com/experis/no/boxinator/models/dto/orderProduct/ShipmentProductWithFullProductDTO.java
@@ -0,0 +1,16 @@
+package com.experis.no.boxinator.models.dto.orderProduct;
+import com.experis.no.boxinator.models.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ShipmentProductWithFullProductDTO {
+    public ShipmentProductWithFullProductDTO(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    private Product product;
+    private int quantity;
+}
