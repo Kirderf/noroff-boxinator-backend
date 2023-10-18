45fb307268ee0998f88e7ad78ce4fa0cd225099f
Fredrik Holm Julsen <Fredrjul@stud.ntnu.no>
2023-10-18 14:08:48 +0200

    ShipmentMapper unmapshipmentProducts

--- src/main/java/com/experis/no/boxinator/models/dto/orderProduct/ShipmentProductDTO.java
+++ src/main/java/com/experis/no/boxinator/models/dto/orderProduct/ShipmentProductDTO.java
@@ -6,11 +6,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ShipmentProductDTO {
-    public ShipmentProductDTO(int productId, int quantity) {
+    public ShipmentProductDTO(int productId, int quantity, int shipmentId) {
+        this.shipmentId = shipmentId;
         this.productId = productId;
         this.quantity = quantity;
     }
-
+    private int shipmentId;
     private int productId;
     private int quantity;
 }
